d6a8498001a92fbf18be353a1149f8e37076ce60
Matteo Dauchez <lekokeliko@gmail.com>
2021-12-10 17:10:29 +0100

    correction fixtures PSR2

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -22,46 +22,46 @@ class AppFixtures extends Fixture
         $this->slugger = $slugger;
         $this->encoder = $encoder;
     }
-    
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
         $faker = Factory::create('fr_FR');
 
-        $admin = new User;
+        $admin = new User();
         $admin->setEmail('dauchez.alain@gmail.com')
-        ->setNom('Dauchez')
-        ->setPrenom('Alain')
-        ->setPassword($this->encoder->hashPassword($admin, 'admin'))
-        ->setAPropos($faker->sentence($nbWords = 40, $variableNbWords = true))
-        ->setTelephone('07 61 92 37 82')
-        ->setRoles(['ROLE_AUTEUR']);
+            ->setNom('Dauchez')
+            ->setPrenom('Alain')
+            ->setPassword($this->encoder->hashPassword($admin, 'admin'))
+            ->setAPropos($faker->sentence($nbWords = 40, $variableNbWords = true))
+            ->setTelephone('07 61 92 37 82')
+            ->setRoles(['ROLE_AUTEUR']);
 
         $manager->persist($admin);
 
-        $categorie = new Categorie;
+        $categorie = new Categorie();
         $categorie->setNom('Sculpture sur bois')
-        ->setDescription($faker->sentence($nbWords = 40, $variableNbWords = true))
-        ->setSlug(strtolower($this->slugger->slug($categorie->getNom())));
+            ->setDescription($faker->sentence($nbWords = 40, $variableNbWords = true))
+            ->setSlug(strtolower($this->slugger->slug($categorie->getNom())));
 
         $manager->persist($categorie);
 
 
-        for($i=0; $i<12; $i++){
-            $sculpture = new Peinture;
+        for ($i = 0; $i < 12; $i++) {
+            $sculpture = new Peinture();
             $sculpture->setNom('Danse')
-            ->setUser($admin)
-            ->setSlug(strtolower($this->slugger->slug($sculpture->getNom())))
-            ->setFile('/img/' . strtolower($sculpture->getNom()) . '/')
-            ->setHauteur(11000)
-            ->setLargeur(11000)
-            ->setPrix(1200000)
-            ->setPortfolio(true)
-            ->setEnVente(true)
-            ->setDescription($faker->sentence($nbWords = 40, $variableNbWords = true))
-            ->setCreatedAt(new DateTime())
-            ->addCategorie($categorie);
+                ->setUser($admin)
+                ->setSlug(strtolower($this->slugger->slug($sculpture->getNom())))
+                ->setFile('/img/' . strtolower($sculpture->getNom()) . '/')
+                ->setHauteur(11000)
+                ->setLargeur(11000)
+                ->setPrix(1200000)
+                ->setPortfolio(true)
+                ->setEnVente(true)
+                ->setDescription($faker->sentence($nbWords = 40, $variableNbWords = true))
+                ->setCreatedAt(new DateTime())
+                ->addCategorie($categorie);
 
             $manager->persist($sculpture);
         }
